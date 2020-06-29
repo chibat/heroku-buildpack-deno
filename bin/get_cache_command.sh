0ce3025d4074cc1d81589f00ca3e30ffdadf6475
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2020-06-29 18:28:38 +0900

    -

---
+++ bin/get_cache_command.sh
@@ -0,0 +1,42 @@
+
+function get_cache_command() {
+  RUN_COMMAND=$1
+  CACHE_OPTION=''
+  OPTION_VALUE=off
+  for WORD in $RUN_COMMAND
+  do
+    if [ "$OPTION_VALUE" = "on" ]
+    then
+      CACHE_OPTION="$CACHE_OPTION $WORD"
+      OPTION_VALUE=off
+      continue
+    fi
+    if [ "$WORD" = "--help" \
+      -o "$WORD" = "--lock-write" \
+      -o "$WORD" = "--no-remote" \
+      -o "$WORD" = "--quiet" \
+      -o "$WORD" = "--unstable" ]
+    then
+      CACHE_OPTION="$CACHE_OPTION $WORD"
+      continue
+    fi
+    if [ "$WORD" = "--cert" \
+      -o "$WORD" = "-c" \
+      -o "$WORD" = "--config" \
+      -o "$WORD" = "--importmap" \
+      -o "$WORD" = "--lock" \
+      -o "$WORD" = "--log-level" ]
+    then
+      CACHE_OPTION="$CACHE_OPTION $WORD"
+      OPTION_VALUE=on
+      continue
+    fi
+    if [[ "$WORD" =~ (.+)\.(t|j)sx?(\"|\')?$ ]]
+    then
+      SCRIPT=$WORD
+      echo "deno cache$CACHE_OPTION $SCRIPT"
+      break
+    fi
+  done
+}
+
---
+++ bin/get_cache_command_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+source $SCRIPT_DIR/get_cache_command.sh
+
+IN='web: deno run foo.ts'
+echo "$IN"
+get_cache_command "$IN"
+echo
+
+IN='web: deno run foo.js'
+echo $IN
+get_cache_command "$IN"
+echo
+
+IN='web: deno run foo.tsx'
+echo $IN
+get_cache_command "$IN"
+echo
+
+IN='web: deno run foo.jsx'
+echo $IN
+get_cache_command "$IN"
+echo
+
+IN='web: deno run --allow-env --allow-net=:${PORT} --unstable -c c.json --config config.json --importmap importmap.json "hoge.ts"'
+echo $IN
+get_cache_command "$IN"
+echo
+
